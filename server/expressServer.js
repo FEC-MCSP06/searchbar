b0f76190836961cba0bb5e7753a83ffbb662f23e
Kolby Evans <77802339+FrootLoopJunkie@users.noreply.github.com>
2021-04-13 14:45:54 -0500

    get specific and get all

--- server/expressServer.js
+++ server/expressServer.js
@@ -2,20 +2,36 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
+const pool = require('./pool');
 const PORT = 3202;
 
 app.use(cors());
 app.use(morgan('combined'));
+app.use(express.json());
 
-// app.get
-
-// app.get
+// get 
+app.get('/api/searchbar', async(req, res) => {
+    try {
+        const id = parseInt(req.body.id);
+        console.log(id)
+        if(id){
+            const videoKey = await pool.query(`SELECT * FROM videos WHERE id = ${id}`);
+            res.status(200).json(videoKey.rows)
+        }else{
+            const videoKeys = await pool.query('SELECT * FROM videos');
+            res.status(200).json(videoKeys.rows)
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).end(err)
+    }
+})
 
 // app.post
 
 // app.put
 
-app.delete
+//app.delete
 
 app.listen(PORT, () => {
     console.log(`Now Listening on Port: ${PORT}`)
