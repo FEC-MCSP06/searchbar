9cba1ec1cf4c860d5a650b2f114869921b9dd1c7
Kolby Evans <77802339+FrootLoopJunkie@users.noreply.github.com>
2021-04-21 11:16:52 -0500

    Changes to App

--- searchbar/client/src/App.js
+++ searchbar/client/src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
 
   render(){
     return (
-      <div className="topbar">
+      <div className="App">
         <SearchbarInput handleSearchButton={this.handleSearchButton}/>
       </div>
     );
